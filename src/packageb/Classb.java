be584e534e05ed0d8a86e83eb8278c09e04a8fbf
Sukruth99 <sunnykotturu@gmail.com>
2024-02-13 15:25:41 -0500

    Initial commit

---
+++ src/packageb/Classb.java
@@ -0,0 +1,10 @@
+package packageb;
+
+import packagea.ClassA;
+
+public class Classb  {
+    public static void main(String[] args) {
+        ClassA a;
+        System.out.println("Got it ");
+    }
+}
